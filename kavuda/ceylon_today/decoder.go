ee9218996486df4a703266d7c1084927e2fcef66
umayanga <umayangag@gmail.com>
2020-06-23 15:45:12 +0530

    ceylon today multiple url crawler

--- kavuda/ceylon_today/decoder.go
+++ kavuda/ceylon_today/decoder.go
@@ -5,8 +5,14 @@ import (
 )
 
 var singleNewsUrl = "https://ceylontoday.lk/news/"
-var newsSiteUrl = "https://ceylontoday.lk/api/category/news?category_id=1&offset=8&time=all"
 var defaultImageUrl = "https://ceylontoday.lk/images/logo.gif"
+var newsSources = []models.NewsSource{
+	{Link: "https://ceylontoday.lk/api/category/news?category_id=1&offset=0&time=all", Categories: []string{"Local"}},
+	{Link: "https://ceylontoday.lk/api/category/news?category_id=3&offset=0&time=all", Categories: []string{"Business"}},
+	{Link: "https://ceylontoday.lk/api/category/news?category_id=4&offset=0&time=all", Categories: []string{"Politics"}},
+	{Link: "https://ceylontoday.lk/api/category/news?category_id=5&offset=0&time=all", Categories: []string{"Sports"}},
+	{Link: "https://ceylontoday.lk/api/category/news?category_id=6&offset=0&time=all", Categories: []string{"Interviews"}},
+}
 
 type CeylonTodayDecoder struct {
 	models.IDecoder
--- kavuda/ceylon_today/extract_news_items.go
+++ kavuda/ceylon_today/extract_news_items.go
@@ -10,37 +10,42 @@ import (
 )
 
 func (d CeylonTodayDecoder) ExtractNewsItems() ([]models.NewsItem, error) {
-	//get the page
-	resp, err := request_handlers.GetRequest(newsSiteUrl)
-	if err != nil {
-		return nil, err
-	}
-	var (
-		newsItemsResponse models2.NewsItemsResponse
-		newsItems         []models.NewsItem
-	)
-	if err := json.Unmarshal([]byte(resp), &newsItemsResponse); err != nil {
-		return nil, err
-	}
+	var allNewsItems []models.NewsItem
 
-	var newsLinks []string
-
-	//create news item list from news item responses
-	for _, newsItemResponse := range newsItemsResponse.Data {
-		url := singleNewsUrl + newsItemResponse.Slug
-		if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
-			newsLinks = append(newsLinks, url)
-			newsItem := models.NewsItem{
-				Link: url,
-				Title:newsItemResponse.Title,
-				Snippet:newsItemResponse.ShortContent,
-				Date:utils.ExtractPublishedDate("2006-01-02 15:04:05", newsItemResponse.PublishDate),
-				Author:newsItemResponse.Author,
-
-			}
-			newsItems = append(newsItems, newsItem)
+	for _, newsSource := range newsSources {
+		//get the page
+		resp, err := request_handlers.GetRequest(newsSource.Link)
+		if err != nil {
+			return nil, err
 		}
+		var (
+			newsItemsResponse models2.NewsItemsResponse
+			newsItems         []models.NewsItem
+		)
+		if err := json.Unmarshal([]byte(resp), &newsItemsResponse); err != nil {
+			return nil, err
+		}
+
+		var newsLinks []string
+
+		//create news item list from news item responses
+		for _, newsItemResponse := range newsItemsResponse.Data {
+			url := singleNewsUrl + newsItemResponse.Slug
+			if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
+				newsLinks = append(newsLinks, url)
+				newsItem := models.NewsItem{
+					Link:    url,
+					Title:   newsItemResponse.Title,
+					Snippet: newsItemResponse.ShortContent,
+					Date:    utils.ExtractPublishedDate("2006-01-02 15:04:05", newsItemResponse.PublishDate),
+					Author:  newsItemResponse.Author,
+					Categories: newsSource.Categories,
+				}
+				newsItems = append(newsItems, newsItem)
+			}
+		}
+		allNewsItems = append(allNewsItems, newsItems...)
 	}
 
-	return newsItems, nil
+	return allNewsItems, nil
 }
