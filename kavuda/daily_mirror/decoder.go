bb5764266f74056b7e4b258ad30ee9874c671f5c
umayanga <umayangag@gmail.com>
2020-06-23 16:04:44 +0530

    daily mirror multiple sources crawler

--- kavuda/daily_mirror/decoder.go
+++ kavuda/daily_mirror/decoder.go
@@ -4,8 +4,12 @@ import (
 	"GIG-Scripts/kavuda/models"
 )
 
-var newsSiteUrl = "http://www.dailymirror.lk/top-storys/155"
 var defaultImageUrl = "http://static.dailymirror.lk/assets/uploads/advr_8570bf3960.jpg"
+var newsSources = []models.NewsSource{
+	{Link: "http://www.dailymirror.lk/top-storys/155", Categories: []string{"Local"}},
+	{Link: "http://www.dailymirror.lk/latest-news/108", Categories: []string{"Local"}},
+	{Link: "http://www.dailymirror.lk/news/209", Categories: []string{"Local"}},
+}
 
 type DailyMirrorDecoder struct {
 	models.IDecoder
