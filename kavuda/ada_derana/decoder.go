635231833d350a0b4d63d80efa7e6b6120c96ef8
umayanga <umayangag@gmail.com>
2020-06-23 15:34:01 +0530

    ada derana crawling multiple urls

--- kavuda/ada_derana/decoder.go
+++ kavuda/ada_derana/decoder.go
@@ -4,8 +4,11 @@ import (
 	"GIG-Scripts/kavuda/models"
 )
 
-var newsSiteUrl = "http://www.adaderana.lk/hot-news/"
 var defaultImageUrl = "http://www.adaderana.lk/newstyle2017/assets/images/adaderana-header-logo-2020.png"
+var newsSources = []models.NewsSource{
+	{Link: "http://www.adaderana.lk/hot-news/", Categories: []string{"Local"}},
+	{Link: "http://www.adaderana.lk/sports-news/", Categories: []string{"Sports"}},
+}
 
 type AdaDeranaDecoder struct {
 	models.IDecoder
--- kavuda/ada_derana/extract_news_items.go
+++ kavuda/ada_derana/extract_news_items.go
@@ -10,41 +10,47 @@ import (
 )
 
 func (d AdaDeranaDecoder) ExtractNewsItems() ([]models.NewsItem, error) {
-	//get the page
-	resp, err := request_handlers.GetRequest(newsSiteUrl)
-	if err != nil {
-		return nil, err
-	}
-	//convert html string to doc for element selection
-	doc, err := libraries.HTMLStringToDoc(resp)
-	if err != nil {
-		return nil, err
-	}
+	var allNewsItems []models.NewsItem
 
-	var newsLinks []string
-
-	newsNodes := doc.Find(".news-story")
-	var newsItems []models.NewsItem
-	for _, node := range newsNodes.Nodes {
-		nodeDoc := goquery.NewDocumentFromNode(node)
-
-		url, _ := nodeDoc.Find("a").First().Attr("href")
-
-		if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
-			newsLinks = append(newsLinks, url)
-
-			title := nodeDoc.Find("a").First().Nodes[0].FirstChild.Data
-			snippet := nodeDoc.Find("p").First().Nodes[0].FirstChild.Data
-			dateString := strings.Replace(nodeDoc.Find("span").First().Nodes[0].FirstChild.Data, "  ", " ", -1) // replacing double &nbsp; characters with space
-
-			newsItems = append(newsItems, models.NewsItem{
-				Title:   title,
-				Link:    url,
-				Date:    utils.ExtractPublishedDate("| January 2, 2006 3:04 pm", dateString),
-				Snippet: snippet,
-			})
+	for _, newsSource := range newsSources {
+		//get the page
+		resp, err := request_handlers.GetRequest(newsSource.Link)
+		if err != nil {
+			return nil, err
 		}
+		//convert html string to doc for element selection
+		doc, err := libraries.HTMLStringToDoc(resp)
+		if err != nil {
+			return nil, err
+		}
+
+		var newsLinks []string
+
+		newsNodes := doc.Find(".news-story")
+		var newsItems []models.NewsItem
+		for _, node := range newsNodes.Nodes {
+			nodeDoc := goquery.NewDocumentFromNode(node)
+
+			url, _ := nodeDoc.Find("a").First().Attr("href")
+
+			if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
+				newsLinks = append(newsLinks, url)
+
+				title := nodeDoc.Find("a").First().Nodes[0].FirstChild.Data
+				snippet := nodeDoc.Find("p").First().Nodes[0].FirstChild.Data
+				dateString := strings.Replace(nodeDoc.Find("span").First().Nodes[0].FirstChild.Data, "  ", " ", -1) // replacing double &nbsp; characters with space
+
+				newsItems = append(newsItems, models.NewsItem{
+					Title:   title,
+					Link:    url,
+					Date:    utils.ExtractPublishedDate("| January 2, 2006 3:04 pm", dateString),
+					Snippet: snippet,
+					Categories:newsSource.Categories,
+				})
+			}
+		}
+		allNewsItems=append(allNewsItems,newsItems...)
 	}
 
-	return newsItems, nil
+	return allNewsItems, nil
 }
