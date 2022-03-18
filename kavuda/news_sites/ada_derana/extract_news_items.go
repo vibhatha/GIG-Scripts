b2919fa12800f9f86c897b65b71656dc5fc566c6
umayanga <umayangag@gmail.com>
2022-03-18 17:29:54 +0530

    refactor duplicate code

--- kavuda/news_sites/ada_derana/extract_news_items.go
+++ kavuda/news_sites/ada_derana/extract_news_items.go
@@ -18,34 +18,41 @@ func (d AdaDeranaDecoder) ExtractNewsItems() ([]models.NewsItem, error) {
 			return nil, err
 		}
 
-		var newsLinks []string
-
-		newsNodes := doc.Find(".news-story")
-		var newsItems []models.NewsItem
-		for _, node := range newsNodes.Nodes {
-			nodeDoc := goquery.NewDocumentFromNode(node)
-
-			url, _ := nodeDoc.Find("a").First().Attr("href")
-
-			if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
-				newsLinks = append(newsLinks, url)
-
-				title := nodeDoc.Find("a").First().Nodes[0].FirstChild.Data
-				snippet := nodeDoc.Find("p").First().Nodes[0].FirstChild.Data
-				dateString := strings.Replace(nodeDoc.Find("span").Last().Nodes[0].FirstChild.Data, "  ", " ", -1) // replacing double &nbsp; characters with space
-				dateString = strings.Replace(dateString, "| ", "", -1)
-
-				newsItems = append(newsItems, models.NewsItem{
-					Title:      title,
-					Link:       url,
-					Date:       helpers.ExtractPublishedDate("January 2, 2006 3:04 pm", dateString),
-					Snippet:    snippet,
-					Categories: newsSource.Categories,
-				})
-			}
-		}
-		allNewsItems = append(allNewsItems, newsItems...)
+		allNewsItems = append(allNewsItems, createNewsItems(doc,newsSource)...)
 	}
 
 	return allNewsItems, nil
+}
+
+func createNewsItems(doc *goquery.Document, newsSource models.NewsSource) []models.NewsItem {
+	var newsLinks []string
+	newsNodes := doc.Find(".news-story")
+	var newsItems []models.NewsItem
+	for _, node := range newsNodes.Nodes {
+		nodeDoc := goquery.NewDocumentFromNode(node)
+
+		url, _ := nodeDoc.Find("a").First().Attr("href")
+		if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
+			newsLinks = append(newsLinks, url)
+
+			newsItems = append(newsItems, createNewsItem(nodeDoc, url, newsSource))
+		}
+	}
+
+	return newsItems
+}
+
+func createNewsItem(nodeDoc *goquery.Document, url string, newsSource models.NewsSource) models.NewsItem {
+	title := nodeDoc.Find("a").First().Nodes[0].FirstChild.Data
+	snippet := nodeDoc.Find("p").First().Nodes[0].FirstChild.Data
+	dateString := strings.Replace(nodeDoc.Find("span").Last().Nodes[0].FirstChild.Data, "  ", " ", -1) // replacing double &nbsp; characters with space
+	dateString = strings.Replace(dateString, "| ", "", -1)
+
+	return models.NewsItem{
+		Title:      title,
+		Link:       url,
+		Date:       helpers.ExtractPublishedDate("January 2, 2006 3:04 pm", dateString),
+		Snippet:    snippet,
+		Categories: newsSource.Categories,
+	}
 }
--- kavuda/news_sites/daily_mirror/extract_news_items.go
+++ kavuda/news_sites/daily_mirror/extract_news_items.go
@@ -17,32 +17,40 @@ func (d DailyMirrorDecoder) ExtractNewsItems() ([]models.NewsItem, error) {
 			return nil, err
 		}
 
-		var newsLinks []string
-
-		newsNodes := doc.Find(".col-md-8")
-		var newsItems []models.NewsItem
-		for _, node := range newsNodes.Nodes {
-			nodeDoc := goquery.NewDocumentFromNode(node)
-
-			url, _ := nodeDoc.Find("a").First().Attr("href")
-			if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
-				newsLinks = append(newsLinks, url)
-
-				dateString := nodeDoc.Find(".gtime").First().Nodes[0].FirstChild.Data
-				title := nodeDoc.Find(".cat-hd-tx").First().Nodes[0].FirstChild.Data
-				snippet := nodeDoc.Find("p").Last().Nodes[0].FirstChild.Data
-
-				newsItems = append(newsItems, models.NewsItem{
-					Title:      title,
-					Snippet:    snippet,
-					Link:       url,
-					Date:       helpers.ExtractPublishedDate("02 Jan 2006 ", dateString),
-					Categories: newsSource.Categories,
-				})
-			}
-		}
-		allNewsItems = append(allNewsItems, newsItems...)
+		allNewsItems = append(allNewsItems, createNewsItems(doc,newsSource)...)
 	}
 
 	return allNewsItems, nil
+}
+
+func createNewsItems(doc *goquery.Document, newsSource models.NewsSource) []models.NewsItem {
+	var newsLinks []string
+	newsNodes := doc.Find(".col-md-8")
+	var newsItems []models.NewsItem
+	for _, node := range newsNodes.Nodes {
+		nodeDoc := goquery.NewDocumentFromNode(node)
+
+		url, _ := nodeDoc.Find("a").First().Attr("href")
+		if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
+			newsLinks = append(newsLinks, url)
+
+			newsItems = append(newsItems, createNewsItem(nodeDoc, url, newsSource))
+		}
+	}
+
+	return newsItems
+}
+
+func createNewsItem(nodeDoc *goquery.Document, url string, newsSource models.NewsSource) models.NewsItem {
+	dateString := nodeDoc.Find(".gtime").First().Nodes[0].FirstChild.Data
+	title := nodeDoc.Find(".cat-hd-tx").First().Nodes[0].FirstChild.Data
+	snippet := nodeDoc.Find("p").Last().Nodes[0].FirstChild.Data
+
+	return models.NewsItem{
+		Title:      title,
+		Snippet:    snippet,
+		Link:       url,
+		Date:       helpers.ExtractPublishedDate("02 Jan 2006 ", dateString),
+		Categories: newsSource.Categories,
+	}
 }
