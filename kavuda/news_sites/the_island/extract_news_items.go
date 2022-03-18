833bbabc8aa9d6970338bc0fd801375266e4ebff
umayanga <umayangag@gmail.com>
2022-03-18 14:09:13 +0530

    the island news extractor tiding up

--- kavuda/news_sites/the_island/extract_news_items.go
+++ kavuda/news_sites/the_island/extract_news_items.go
@@ -5,7 +5,9 @@ import (
 	"GIG-Scripts/global_helpers"
 	"GIG-Scripts/kavuda/helpers"
 	"GIG-Scripts/kavuda/models"
+	"errors"
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
 func (d TheIslandDecoder) ExtractNewsItems() ([]models.NewsItem, error) {
@@ -23,30 +25,38 @@ func (d TheIslandDecoder) ExtractNewsItems() ([]models.NewsItem, error) {
 
 		var newsItems []models.NewsItem
 		for _, node := range newsNodes.Nodes {
-			nodeDoc := goquery.NewDocumentFromNode(node)
-			dateString, _ := nodeDoc.Find(".mvp-cd-date").First().Html()
-
-			if dateString != "" {
-				extractedUrl, _ := nodeDoc.Find("a").First().Attr("href")
-				if extractedUrl != "/" {
-					title := nodeDoc.Find("h2").First().Text()
-					url := libraries.FixUrl(extractedUrl, newsSource.Link)
-
-					if !libraries.StringInSlice(newsLinks, url) { // if the link is not already enlisted before
-						newsLinks = append(newsLinks, url)
-
-						newsItems = append(newsItems, models.NewsItem{
-							Title:      title,
-							Link:       url,
-							Date:       helpers.ExtractPublishedDate("January 2, 2006, 3:04 pm", dateString),
-							Categories: newsSource.Categories,
-						})
-					}
-				}
+			newsItem, url, err := generateNewsItem(node, newsSource)
+			if !libraries.StringInSlice(newsLinks, url) && err == nil { // if the link is not already enlisted before
+				newsLinks = append(newsLinks, url)
+				newsItems = append(newsItems, newsItem)
 			}
 		}
 		allNewsItems = append(allNewsItems, newsItems...)
 	}
 
 	return allNewsItems, nil
+}
+
+func generateNewsItem(node *html.Node, newsSource models.NewsSource) (models.NewsItem, string, error) {
+	nodeDoc := goquery.NewDocumentFromNode(node)
+	dateString, _ := nodeDoc.Find(".mvp-cd-date").First().Html()
+
+	if dateString != "" {
+		extractedUrl, _ := nodeDoc.Find("a").First().Attr("href")
+		if extractedUrl != "/" {
+			title := nodeDoc.Find("h2").First().Text()
+			url := libraries.FixUrl(extractedUrl, newsSource.Link)
+
+			newsItem := models.NewsItem{
+				Title:      title,
+				Link:       url,
+				Date:       helpers.ExtractPublishedDate("January 2, 2006, 3:04 pm", dateString),
+				Categories: newsSource.Categories,
+			}
+			return newsItem, url, nil
+
+		}
+	}
+
+	return models.NewsItem{}, "", errors.New("news item not found")
 }
