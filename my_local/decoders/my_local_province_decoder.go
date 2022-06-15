cdcd55defc5f5b652a2da6a71542ce6fbe35b419
umayanga <umayangag@gmail.com>
2022-06-15 17:14:04 +0530

    append provinces to country

--- my_local/decoders/my_local_province_decoder.go
+++ my_local/decoders/my_local_province_decoder.go
@@ -1,14 +1,22 @@
 package decoders
 
 import (
+	GIG_Scripts "GIG-Scripts"
 	"GIG-Scripts/my_local/data_models"
 	"github.com/lsflk/gig-sdk/models"
+	"log"
 )
 
 const GeoDataSource = "gig-data-master/geo/province/"
 
 type MyLocalProvinceDecoder struct {
 	MyLocalDecoder
+}
+
+type Payload struct {
+	Title     string       `json:"title"`
+	Attribute string       `json:"attribute"`
+	Value     models.Value `json:"value"`
 }
 
 func (d MyLocalProvinceDecoder) DecodeToEntity(record []string, source string) models.Entity {
@@ -19,5 +27,16 @@ func (d MyLocalProvinceDecoder) DecodeToEntity(record []string, source string) m
 		SetParent("Sri Lanka", source).
 		SetGeoCoordinates(GeoDataSource + record[1] + ".json").
 		AddCategory("Province").AddLink(models.Link{Title: "Sri Lanka"})
+
+	//update parent entity
+	payload := Payload{
+		Title:     "Sri Lanka",
+		Attribute: "provinces",
+		Value:     *new(models.Value).SetSource(source).SetValueString(entity.GetTitle()),
+	}
+	if _, err := GIG_Scripts.GigClient.PostRequest(GIG_Scripts.GigClient.ApiUrl+"append", payload); err != nil {
+		log.Fatal("error updating parent entity", err)
+	}
+
 	return entity
 }
