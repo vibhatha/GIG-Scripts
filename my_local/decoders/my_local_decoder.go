69f0380586317f0e895e16387265f6499a0cbd13
umayanga <umayangag@gmail.com>
2022-06-19 01:11:20 +0530

    optimize requests with memo

--- my_local/decoders/my_local_decoder.go
+++ my_local/decoders/my_local_decoder.go
@@ -7,6 +7,8 @@ import (
 	"log"
 )
 
+var entityMemo = map[string]models.Entity{}
+
 type MyLocalDecoderInterface interface {
 	DecodeToEntity(record []string, source string) models.Entity
 }
@@ -25,12 +27,22 @@ type MyLocalLocationDecoder struct {
 }
 
 func (d MyLocalLocationDecoder) GetParentEntity() models.Entity {
+
+	// check if entity is already loaded in memory - this helps to avoid multiple request to fetch same entity
+	if entity, found := entityMemo[d.ParentId]; found {
+		log.Println("Parent entity found in memo.")
+		return entity
+	}
+
+	// get the entity from server
+	log.Println("Requesting parent entity from server")
 	parentEntity, err := GIG_Scripts.GigClient.GetEntityByAttribute("attributes.location_id", d.ParentId)
 
 	if err != nil {
 		log.Fatal("error getting parent entity:", d.ParentId)
 	}
-
+	// save the entity to memory
+	entityMemo[d.ParentId] = parentEntity
 	return parentEntity
 }
 
